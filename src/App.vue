370b0eb8d5402f2bbffd42b6c4d142d384ce1b1e
L <1053801783@qq.com>
2019-08-06 20:02:24 +0800

    进度条基本完成

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Progress :completeness="0.5"/>
+    <Progress :progress="0.8"/>
   </div>
 </template>
 
--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -1,25 +1,42 @@
 <template>
-  <svg :height="200" :width="200" x-mlns="http://www.w3.org/200/svg">
+  <svg
+    :height="option.size"
+    :width="option.size"
+    x-mlns="http://www.w3.org/200/svg"
+  >
     <circle
-      :r="50"
-      :cx="100"
-      :cy="100"
-      :stroke="'red'"
-      :stroke-width="10"
-      fill="none"/>
+      :r="option.radius"
+      :cx="option.cx"
+      :cy="option.cy"
+      :stroke="option.backColor"
+      :stroke-width="option.strokeWidth"
+      fill="none"
+    />
+    <circle
+      :r="option.radius"
+      :cx="option.cx"
+      :cy="option.cy"
+      :stroke="option.progressColor"
+      :stroke-dasharray="arcLength"
+      :stroke-width="option.strokeWidth"
+      fill="none"
+      transform="rotate(-90)"
+      transform-origin="center"
+      stroke-linecap="round"
+    />
   </svg>
 </template>
 <script>
 export default {
   name: 'Progress',
   props: {
-    completeness: {
+    progress: {
       type: Number,
       required: true,
     },
     progressOption: {
       type: Object,
-      default: () => {},
+      default: () => { },
     },
   },
   data () {
@@ -27,8 +44,25 @@ export default {
     }
   },
   computed: {
+    arcLength () {
+      let circleLength = Math.floor(2 * Math.PI * this.option.radius)
+      let progressLength = this.progress * circleLength
+      return `${progressLength},100000000`
+    },
+    option () {
+      // 所有进度条的可配置项
+      let baseOption = {
+        radius: 100,
+        strokeWidth: 20,
+        backColor: 'red',
+        progressColor: 'yellow',
+      }
+      Object.assign(baseOption, this.progressOption)
+      // 中心位置自动生成
+      baseOption.cy = baseOption.cx = baseOption.radius + baseOption.strokeWidth
+      baseOption.size = (baseOption.radius + baseOption.strokeWidth) * 2
+      return baseOption
+    },
   },
 }
 </script>
-<style scoped lang='stylus'>
-</style>
