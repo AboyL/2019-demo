050857ddee31d4b3a5ff2e514042b695064931d3
L <1053801783@qq.com>
2019-08-07 19:37:19 +0800

    渐变效果完成

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -4,18 +4,6 @@
     :width="option.size"
     x-mlns="http://www.w3.org/200/svg"
   >
-    <defs>
-      <linearGradient id="gradient">
-        <stop
-          offset="0%"
-          style="stop-color: green;"
-        />
-        <stop
-          offset="100%"
-          style="stop-color: yellow"
-        />
-      </linearGradient>
-    </defs>
     <circle
       :r="option.radius"
       :cx="option.cx"
@@ -25,11 +13,13 @@
       fill="none"
     />
     <circle
+      v-for="(item, index) in arcArr"
+      :key="index"
       :r="option.radius"
       :cx="option.cx"
       :cy="option.cy"
-      :stroke="'url(#gradient)'"
-      :stroke-dasharray="arcLength"
+      :stroke="item.color"
+      :stroke-dasharray="item.arcLength"
       :stroke-width="option.strokeWidth"
       fill="none"
       transform="rotate(-90)"
@@ -51,23 +41,45 @@ export default {
       default: () => { },
     },
   },
-  data () {
-    return {
-    }
-  },
   computed: {
-    arcLength () {
+    arcArr () {
       let circleLength = Math.floor(2 * Math.PI * this.option.radius)
       let progressLength = this.progress * circleLength
-      return `${progressLength},100000000`
+      const step = 100 // 设置到100则已经比较难看出来颜色断层
+      const gradientColor = (startRGB, endRGB, step) => {
+        let startR = startRGB[0]
+        let startG = startRGB[1]
+        let startB = startRGB[2]
+        let endR = endRGB[0]
+        let endG = endRGB[1]
+        let endB = endRGB[2]
+        let sR = (endR - startR) / step // 总差值
+        let sG = (endG - startG) / step
+        let sB = (endB - startB) / step
+        let colorArr = []
+        for (let i = 0; i < step; i++) {
+          let color = `rgb(${sR * i + startR},${sG * i + startG},${sB * i + startB})`
+          colorArr.push(color)
+        }
+        return colorArr
+      }
+      let colorArr = gradientColor(this.option.startColor, this.option.endColor, step)
+      // 计算每个步进中的弧长
+      let arcLengthArr = colorArr.map((color, index) => ({
+        arcLength: `${index * (progressLength / 100)},100000000`,
+        color: color
+      }))
+      arcLengthArr.reverse()
+      return arcLengthArr
     },
     option () {
       // 所有进度条的可配置项
       let baseOption = {
         radius: 100,
         strokeWidth: 20,
-        backColor: 'red',
-        progressColor: 'yellow',
+        backColor: '#E6E6E6',
+        startColor: [249, 221, 180],
+        endColor: [238, 171, 86], // 用于渐变色的开始
       }
       Object.assign(baseOption, this.progressOption)
       // 中心位置自动生成
