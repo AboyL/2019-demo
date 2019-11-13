cb4836d73df7ff22d070d996f1d0c4921c1fbc49
L <1053801783@qq.com>
2019-11-13 21:58:10 +0800

    新增示例

--- rxjs-learn/exmaple.ts
+++ rxjs-learn/exmaple.ts
@@ -1,6 +1,46 @@
-import { interval } from "rxjs";
+import { interval, Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
-interval(1000).pipe(
-  map(x => x * 10)
-).subscribe(x => console.log(x))
+// interval(1000).pipe(
+//   map(x => x * 10)
+// ).subscribe(x => console.log(x))
+
+// const test$ = new Observable(subscribe => {
+//   subscribe.next(1)
+//   subscribe.next(2)
+//   subscribe.next(3)
+//   if (Math.random() > 0.9) {
+//     subscribe.complete()
+//   } else {
+//     throw "1111";
+//   }
+// })
+// test$.subscribe({
+//   next(v) {
+//     console.log(v)
+//   },
+//   complete() {
+//     console.log('complete')
+//   },
+//   error(err) {
+//     console.log('err', err)
+//   }
+// })
+
+const test2$ = new Observable(subscribe => {
+  let count = 0
+  setInterval(() => {
+    subscribe.next(++count)
+  }, 1000)
+})
+test2$.subscribe({
+  next(v) {
+    console.log(v)
+  },
+  complete() {
+    console.log('complete')
+  },
+  error(err) {
+    console.log('err', err)
+  }
+})
